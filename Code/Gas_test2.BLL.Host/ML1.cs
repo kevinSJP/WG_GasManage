792c762b51a3bfd5ab97c1859a1f1a9a6f1aca3c
s20141478 <1204214469@qq.com>
2015-07-06 17:11:05 +0800

    change1

---
+++ Code/Gas_test2.BLL.Host/ML1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MLApp;
+namespace Gas_test2.BLL
+{
+    class ML1
+    {
+        public double[,] ExeAlgorithm(double[] inputData, string algType)
+        {
+            MLAppClass matlab1 = new MLAppClass();
+            double[,] outputData = new double[inputData.Length, 1];
+
+            if (algType == "bp")
+            {
+                matlab1.PutWorkspaceData("a", "base", inputData);
+                matlab1.Execute("b=funBP(a)");
+
+                var b = matlab1.GetVariable("b", "base");
+
+                outputData = (double[,])b;
+            }
+            return outputData;
+
+        }
+    }
+}
